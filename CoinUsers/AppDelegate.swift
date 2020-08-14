d16d712c9c0acda18d76e8cb6fd92e4839d782e7
ABCWare <96133128+ABCWare@users.noreply.github.com>
2020-08-14 08:37:46 +0000

    ext4: grab missed write_count for EXT4_IOC_SWAP_BOOT

--- CoinUsers/AppDelegate.swift
+++ CoinUsers/AppDelegate.swift
@@ -9,4 +9,4 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-	func 
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIAp
