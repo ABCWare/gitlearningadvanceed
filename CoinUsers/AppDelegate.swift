2e0b2c2ad18f0f976da26e85798615a3575e37af
ABCWare <96133128+ABCWare@users.noreply.github.com>
2020-08-15 11:32:09 +0000

    Update how-to.yml

--- CoinUsers/AppDelegate.swift
+++ CoinUsers/AppDelegate.swift
@@ -16,4 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	// MARK: UISceneSession Lifecycle
 
-	func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UIScene
+	func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+		// Called when a new scene session is being created.
+		// Use
