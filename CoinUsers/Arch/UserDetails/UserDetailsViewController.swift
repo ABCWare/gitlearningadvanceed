ff101a6d02005b1baae910992b53001023b60bef
ABCWare <96133128+ABCWare@users.noreply.github.com>
2020-12-31 19:10:37 +0000

    added middleware

--- CoinUsers/Arch/UserDetails/UserDetailsViewController.swift
+++ CoinUsers/Arch/UserDetails/UserDetailsViewController.swift
@@ -78,4 +78,8 @@ final class UserDetailsViewController: BaseViewController {
 	func callTo(phoneNumber: String) {
 		if !phoneNumber.isEmpty, let encoded = phoneNumber.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) {
 			if let url = NSURL(string: "tel://" + encoded) {
-				if UIApplication.shared.canOpenURL(url as
+				if UIApplication.shared.canOpenURL(url as URL) {
+					UIApplication.shared.open(url as URL, options: [:], completionHandler: nil)
+				}
+			}
+		}
