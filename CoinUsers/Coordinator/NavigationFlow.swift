9698373562343b8d9e0ba26d9872b1b206263db5
ABCWare <96133128+ABCWare@users.noreply.github.com>
2021-07-04 07:21:23 +0000

    Fix whitespace

--- CoinUsers/Coordinator/NavigationFlow.swift
+++ CoinUsers/Coordinator/NavigationFlow.swift
@@ -34,4 +34,5 @@ class NavigationFlow: Flow {
 	// MARK: - Push
 
 	func push<C: BaseViewController>(to viewController: C.Type) -> FlowContributors {
-		let viewController = self.assembler.resolver
+		let viewController = self.assembler.resolver ~> viewController.self
+		self.rootViewController.pushViewController(viewControlle
