f5de6e0a42010aafc0032266f86af4807d3bbd24
ABCWare <96133128+ABCWare@users.noreply.github.com>
2022-04-10 23:00:43 +0000

    Minor code cleaning.

--- CoinUsers/Models/User/Address.swift
+++ CoinUsers/Models/User/Address.swift
@@ -5,4 +5,9 @@
 //  Created by Hovak Davtyan on 11.05.21.
 //
 
-import Foun
+import Foundation
+
+protocol Address {
+	var country: String { get }
+	var state: String { get }
+	var city: String { get
