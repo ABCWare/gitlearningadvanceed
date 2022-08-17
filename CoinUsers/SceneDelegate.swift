504a28ee337c5065452e89e2516ecbe7e56d16de
ABCWare <96133128+ABCWare@users.noreply.github.com>
2022-08-17 16:48:53 +0000

    from trunk

--- CoinUsers/SceneDelegate.swift
+++ CoinUsers/SceneDelegate.swift
@@ -34,4 +34,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 	}
 
 	func sceneDidDisconnect(_ scene: UIScene) {
-		// Called 
+		// Called as the scene is being released by the system.
+		// This occurs shortly after t
