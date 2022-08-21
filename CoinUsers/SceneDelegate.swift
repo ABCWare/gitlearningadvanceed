f24ed06d9a2ee574c5b65a7743a794d6db201a9e
ABCWare <96133128+ABCWare@users.noreply.github.com>
2022-08-21 08:15:58 +0000

    Update of TR report from new reffy run

--- CoinUsers/SceneDelegate.swift
+++ CoinUsers/SceneDelegate.swift
@@ -35,4 +35,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 	func sceneDidDisconnect(_ scene: UIScene) {
 		// Called as the scene is being released by the system.
-		// This occurs shortly after t
+		// This occurs shortly after the scene enters the background, or when its session is discarded.
+		// Release any resources
