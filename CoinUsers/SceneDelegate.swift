1073ef4ea9b26512f10aa9baf038924cd10bc649
ABCWare <96133128+ABCWare@users.noreply.github.com>
2022-08-31 15:42:13 +0000

    Modify UEFI anti-bricking code

--- CoinUsers/SceneDelegate.swift
+++ CoinUsers/SceneDelegate.swift
@@ -56,4 +56,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 	}
 
 	func sceneDidEnterBackground(_ scene: UIScene) {
-		// Called as the scene t
+		// Called as the scene transitions from the foreground to the background.
+		// Use this method to save data, release sh
