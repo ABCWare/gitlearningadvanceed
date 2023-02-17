78fd79b944d9d184774304467233cd8b8ff9f332
ABCWare <96133128+ABCWare@users.noreply.github.com>
2023-02-17 23:06:59 +0000

    Update ex_doc

--- CoinUsersUITests/CoinUsersUITests.swift
+++ CoinUsersUITests/CoinUsersUITests.swift
@@ -10,4 +10,4 @@ import XCTest
 class CoinUsersUITests: XCTestCase {
 
     override func setUpWithError() throws {
-        // 
+        // Put setup code here. This method is called before the invocation of each test method in the class
