b16ef2189d987d69d7b06edc216938a85e1df867
ABCWare <96133128+ABCWare@users.noreply.github.com>
2023-02-19 14:43:54 +0000

    Comment code

--- CoinUsersUITests/CoinUsersUITests.swift
+++ CoinUsersUITests/CoinUsersUITests.swift
@@ -15,4 +15,4 @@ class CoinUsersUITests: XCTestCase {
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
 
-        // In UI test
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your te
