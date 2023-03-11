7993dae67c02c9faff15e3c3aaebee1d7734b640
ABCWare <96133128+ABCWare@users.noreply.github.com>
2023-03-11 18:30:15 +0000

    Added Polish translation

--- CoinUsersUITests/CoinUsersUITests.swift
+++ CoinUsersUITests/CoinUsersUITests.swift
@@ -33,4 +33,5 @@ class CoinUsersUITests: XCTestCase {
 
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how 
+            // This measures how long it takes to launch your application.
+            measure(metrics: [XCTApplicationL
