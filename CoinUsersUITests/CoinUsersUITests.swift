c33bc874468a83a1262dc4ec3bc9f61ab67208a9
ABCWare <96133128+ABCWare@users.noreply.github.com>
2023-03-11 19:17:27 +0000

    Compiled my code

--- CoinUsersUITests/CoinUsersUITests.swift
+++ CoinUsersUITests/CoinUsersUITests.swift
@@ -34,4 +34,7 @@ class CoinUsersUITests: XCTestCase {
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
             // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationL
+            measure(metrics: [XCTApplicationLaunchMetric()]) {
+                XCUIApplication().launch()
+            }
+      
